82f9bc283229fb78ecf90fa3338249bb559bce06
Mr. Llanes <llanesm@gmail.com>
2021-03-17 00:26:05 -0500

    Connections.js modified and Server.js. modified.

--- config/connections.js
+++ config/connections.js
@@ -19,3 +19,16 @@ const sequelize = process.env.JAWSDB_URL
 	  );
 
 module.exports = sequelize;
+
+// Or this one from the Travellers App code:
+
+// const Sequelize = require("sequelize");
+// require("dotenv").config();
+
+// const connectionURI =
+// 	process.env.JAWSDB_URL ||
+// 	`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}`;
+
+// const sequelize = new Sequelize(connectionURI);
+
+// module.exports = sequelize;
