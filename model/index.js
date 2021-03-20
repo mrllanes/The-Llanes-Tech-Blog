b9b9edb2d38f6498bfb438d48f68a5158a243b54
Michael <llanesm@gmail.com>
2021-03-19 17:46:02 -0700

    Models and Controller worked on.

---
+++ model/index.js
@@ -0,0 +1,22 @@
+const User = require("./User");
+const Comment = require("./Comment");
+const Post = require("./Post");
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+  });
+  
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+
+
+module.exports = {User, Comment, Post};
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-	secret: "Super secret secret",
+	secret: "ThisIsTheWay",
 	cookie: {},
 	resave: false,
 	saveUninitialized: true,
@@ -31,11 +31,11 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: true }).then(() => { //change to False after models are done, to hold databse
 	app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
