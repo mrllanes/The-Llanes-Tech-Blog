5218984ba41e4d43617f1a4928559642f8e9fb4c
Michael <llanesm@gmail.com>
2021-03-21 00:21:05 -0700

    Handlebar files and templates created.

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require("./api");
 const homeRoutes = require("./homeroutes");
 const dashboardRoutes = require("./dashboardroutes");
 
-// router.use("/", homeRoutes);
+router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 // router.use("/dashboard", dashboardRoutes);
 
